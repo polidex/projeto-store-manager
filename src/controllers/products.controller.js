c9144b68f7b744f5019a8fa47d4d206bb8d56310
polidex <lp.igor@gmail.com>
2022-09-30 11:07:53 -0300

    fix: reqt 1

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,15 +1,17 @@
 const productsService = require('../services/products.service');
 
 const listProducts = async (_req, res) => {
-  const productsList = productsService.getDbProducts();
+  const productsList = await productsService.getDbProducts();
   return res.status(200).json(productsList);
 };
 
 const listProductsById = async (req, res) => {
   const { id } = req.params;
-  const productsListById = productsService.getDbProductsById(id);
-  if (!productsListById) return res.status(404).json('Product not found');
-  return res.status(200).json(productsListById);
+  const productsListById = await productsService.getDbProductsById(id);
+  if (productsListById.length === 0) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  return res.status(200).json(productsListById[0]);
 };
 
 module.exports = {
--- src/models/products.model.js
+++ src/models/products.model.js
@@ -3,13 +3,13 @@ const connection = require('../db/connection');
 const readDbProducts = async () => {
   const query = 'SELECT * FROM StoreManager.products';
   const result = await connection.execute(query);
-  return result;
+  return result[0];
 };
 
 const readDbProductsById = async (id) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
   const result = await connection.execute(query, [id]);
-  return result;
+  return result[0];
 };
 
 module.exports = {
